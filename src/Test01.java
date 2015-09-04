c9739c5a571f479dbd17d8af766c9c10fa14625a
unknown <x15g029@PC505N018.chiba-fjb.ac.jp>
2015-09-04 12:03:36 +0900

    henkou

--- src/Test01.java
+++ src/Test01.java
@@ -4,6 +4,9 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("お昼ごはん");
+		System.out.println("焼きおにぎり");
+		System.out.println("炒飯");
+		System.out.println("プリン");
 		
 	}
 
