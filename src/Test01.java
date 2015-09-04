2dcb2a00a190e900a8d91345001c042f3f8e688d
unknown <x15g029@PC505N018.chiba-fjb.ac.jp>
2015-09-04 12:08:32 +0900

    消しました

--- src/Test01.java
+++ src/Test01.java
@@ -3,12 +3,7 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("お昼ごはん");
-		System.out.println("焼きおにぎり");
-		System.out.println("炒飯");
-		System.out.println("プリン");
-		System.out.println("ラーメン");
-		System.out.println("つけめん");
+		;
 	}
 
 }
